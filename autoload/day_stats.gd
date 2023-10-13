022ab7816d8ff99d0c409ca4f164bdf1ab817a65
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-13 20:39:27 -0300

    Basic test on switching day to night
    Updated code accordingly, button calls upon change_day global signal, which then triggers change_day function

--- autoload/day_stats.gd
+++ autoload/day_stats.gd
@@ -1,11 +1,34 @@
 extends Node
 
+enum DayType {DAY, NIGHT} 
+@export var current_time : DayType = DayType.DAY :
+	get: 
+		return current_time
+	set(value): 
+		current_time = value
+	
+@export var days_passed : int = 0 : 
+	set(value): days_passed = value
+	get: return days_passed 
+@export var night_passed : int = 0 :
+	set(value): night_passed = value
+	get: return night_passed 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	GlobalSignals.change_day.connect(_change_day)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _change_day():
+	if current_time == DayType.DAY:
+		days_passed += 1
+		current_time = DayType.NIGHT
+	else:
+		night_passed += 1
+		current_time = DayType.DAY
+	print(current_time)
