0772400358d05d636dd62b62dfff0885c5cd1ab6
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-13 20:57:46 -0300

    Updated floor & created floor_properties class

--- Floor/floor.gd
+++ Floor/floor.gd
@@ -1,11 +1,20 @@
 extends Node2D
 
+@export var floor_properties : FloorProperty = preload("res://Floor/Resources/Default.tres")
+@export var plant_used : PlantProperties = preload("res://Plant/Resources/no_plant.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	GlobalSignals.day_changed.connect(_update_seeds_new_day)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _update_seeds_new_day():
+	if plant_used.plant_current_age == plant_used.plant_max_age:
+		pass # Kill plant
+	else:
+		pass 
---
+++ Floor/floor_properties.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name FloorProperty
+
+@export var plant_used : PlantProperties = preload("res://Plant/Resources/no_plant.tres")
+@export var number : int = -1
+@export var price : int = -1
