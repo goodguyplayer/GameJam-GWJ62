05e7cc8bd6dd87a34dee50d0c8a1e16331d29c60
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-13 20:14:08 -0300

    Made basic plant properties that I think will be used

--- Plant/plant_properties.gd
+++ Plant/plant_properties.gd
@@ -1,1 +1,9 @@
 extends Resource
+class_name PlantProperties
+
+@export var plant_name: String
+@export var seed_used : String
+@export var plant_type : String
+@export var time_growth : int
+@export var humans_needed : int 
+@export var blood_needed : int
