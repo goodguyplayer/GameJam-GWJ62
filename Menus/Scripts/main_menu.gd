2966c37aa6e8fe76b06f39f6bde7f0de70a248b1
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-14 19:49:00 -0300

    Menu & Credits - Updated
    Button logic

---
+++ Menus/Scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_start_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_pressed():
+	get_tree().change_scene_to_file("res://Menus/credits.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
