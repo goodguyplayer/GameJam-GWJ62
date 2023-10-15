49634e3f6c11e19fbe13128593b2f78a484f8757
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-14 23:21:02 -0300

    Pause menu fix process and code
    Here's hoping it works now

--- Menus/Pause/pause.gd
+++ Menus/Pause/pause.gd
@@ -11,7 +11,7 @@ func _process(delta):
 	pass
 
 func _input(event):
-	if event.is_action("ui_pause"):
+	if event.is_action_pressed("ui_pause"):
 		var new_pause_state = not get_tree().paused
 		get_tree().paused = new_pause_state
 		visible = new_pause_state
