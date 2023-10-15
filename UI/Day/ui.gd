860d1d0029e66a002b7d920d12d1d63192827c5b
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-14 23:20:08 -0300

    UI update, code for buttons

---
+++ UI/Day/ui.gd
@@ -0,0 +1,26 @@
+extends CanvasLayer
+
+@onready var money_number = $MarginContainer/Header/HBoxContainer/Money/VBoxContainer/MoneyNumber
+@onready var days_number = $MarginContainer/Header/HBoxContainer/Days/VBoxContainer/DaysNumber
+@onready var score_number = $MarginContainer/Header/HBoxContainer/Score/VBoxContainer/ScoreNumber
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalSignals.day_changed.connect(_on_new_day)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_new_day():
+	days_number.text = str(DayStats.days_passed)
+
+
+func _on_log_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_end_day_pressed():
+	GlobalSignals.change_day.emit()
