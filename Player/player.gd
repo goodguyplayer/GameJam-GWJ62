63e5cb445336078eb67adcca3eb29c868dd2ceee
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-17 23:14:38 -0200

    Weapon player updated - Should add zombies later
    And fix the way weapon fires

---
+++ Player/player.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@onready var player_melee = $PlayerMelee
+@onready var player_ranged = $PlayerRanged
+var current
+var day_time_flag = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalSignals.day_time.connect(day_time)
+	GlobalSignals.night_time.connect(night_time)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = get_global_mouse_position()
+	if not day_time_flag:
+		if Input.is_action_pressed("mouse_click"):
+			current.attack()
+
+
+func night_time(melee_ranged):
+	if melee_ranged == 0: # Melee
+		current = melee_ranged
+		player_melee.visible = (true)
+	else:
+		current = player_ranged
+		player_ranged.visible = (true)
+		
+
+func day_time():
+	player_melee.visible = false
+	player_ranged.visible = false
--- autoload/global_signals.gd
+++ autoload/global_signals.gd
@@ -3,3 +3,5 @@ extends Node
 signal day_changed() # Get current time (necessary?)
 signal change_day() # Switch day to night and night to day
 signal change_theme_font(theme_font)
+signal day_time()
+signal night_time(weapon_of_choice)
