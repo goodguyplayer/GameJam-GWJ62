29dcb4ec8b481e77d828bb9a827bfd5a5ae58320
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-18 23:52:43 -0200

    Update - Build

--- Menus/Scripts/main_menu.gd
+++ Menus/Scripts/main_menu.gd
@@ -1,5 +1,21 @@
 extends Control
 
+@onready var build_version = $Title/MarginContainer/HBoxContainer2/HBoxContainer/Subtitle2
+
+func _ready():
+	$Buttons/MarginContainer/VBoxContainer/Start.grab_focus()
+	if OS.has_feature("debug"):
+		build_version.text = "Debug mode activated"
+	elif OS.has_feature("release"):
+		if OS.has_feature("Windows"):
+			build_version.text = "v0.1 - Build Version - Windows - Boring but works"
+		if OS.has_feature("X11"):
+			build_version.text = "v0.1 - Build Version - Linux - I don't use Arch BTW"
+		if OS.has_feature("OSX"):
+			build_version.text = "v0.1 - Build Version - macOS - But why tho"
+		if OS.has_feature("HTML5"):
+			build_version.text = "v0.1 - Build Version - Web Browser - As long as it works..."
+
 
 
 func _on_start_pressed():
