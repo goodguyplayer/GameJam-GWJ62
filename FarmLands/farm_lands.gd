2f252bdb8779208700ff6e0a17091c49c712f71f
Nathan B <42227590+goodguyplayer@users.noreply.github.com>
2023-10-19 22:57:25 -0200

    Added basic night timer

--- FarmLands/farm_lands.gd
+++ FarmLands/farm_lands.gd
@@ -1,4 +1,15 @@
 extends Node2D
 
+@onready var timer = $Timer
+@export var timer_night = 5
 
-# Called when the node enters the scene tree for the first time.
+func _ready():
+	GlobalSignals.night_time.connect(_night_time_warzone)
+
+
+func _night_time_warzone():
+	timer.start(timer_night)
+
+
+func _on_timer_timeout():
+	GlobalSignals.day_time.emit()
